e4efcbcf01cf3ee1c64b98ed4ceb3773ba87ddf5
Anas Shahid <muhammad.shahid@ericsson.com>
2021-09-24 11:25:42 -0400

    updated the index with solclient

--- src/components/onlineUsers/OnlineUser.jsx
+++ src/components/onlineUsers/OnlineUser.jsx
@@ -7,14 +7,16 @@ const messagingService = new MessagingService();
 export default function OnlineUsers() {
   const messageService = useRef(messagingService);
   const [onlineUsers, setOnlineUsers] = useState([]);
+  const [msg, setMsg] = useState("");
 
   useEffect(() => {
     const connectToBroker = async () => await messageService.current.connect();
     const cleanup = () => messageService.current.disconnect();
     connectToBroker();
-    messageService.current.subscribeToTopic("onlineUsers", setOnlineUsers);
+    messageService.current.subscribeToTopic("onlineUsers", setMsg);
+    setOnlineUsers((oldArray) => [...oldArray, msg]);
     return cleanup;
-  }, [messageService]);
+  }, [messageService, msg]);
   return (
     <div className="onlineUsers">
       <MessagingServiceContext.Provider value={messagingService}>
