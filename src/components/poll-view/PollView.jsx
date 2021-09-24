42e9edc135837b43f39c718d1b095b466f860ab9
omostafa <omar-125@live.com>
2021-09-24 15:50:23 -0400

    adding hardcoded ids

--- src/components/poll-view/PollView.jsx
+++ src/components/poll-view/PollView.jsx
@@ -5,9 +5,15 @@ import AddPoll from "../../static/add-poll.svg";
 import BarChart from "../../static/bar-chart.svg";
 
 const PollView = (props) => {
+
     function CreateNewPoll() {
         console.log("Create new Poll");
     }
+
+    function getId(event) {
+        console.log(event.target.id);
+    }
+
   return (
     <div class="main-div">
         <img src={BarChart} alt="bar chart svg"></img>
@@ -16,19 +22,18 @@ const PollView = (props) => {
             Create new poll
         </button>
         <div className="box">
-            <div class = "box-text"> Poll #1</div>
+            <div id = "Poll1" class = "box-text" onClick={event => getId(event)}> Poll #1</div>
         </div>
         <div className="box">
-            <div class = "box-text"> Poll #2</div>
+            <div id = "Poll2" class = "box-text" onClick={event => getId(event)}> Poll #2</div>
         </div>
         <div className="box">
-            <div class = "box-text"> Poll #3</div>
+            <div id = "Poll3" class = "box-text" onClick={event => getId(event)}> Poll #3</div>
         </div>
         <div className="box">
-            <div class = "box-text"> Poll #4</div>
+            <div id = "Poll4" class = "box-text" onClick={event => getId(event)}> Poll #4</div>
         </div>
-    </div>  
-   
+    </div> 
   );
 };
 export default PollView;
