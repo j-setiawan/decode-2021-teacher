21418a137db426c66177833f1be02359bac35a1b
Kevin Shao <kevin.shao@solace.com>
2021-09-24 17:39:47 -0400

    Add stuff

--- src/components/storybook/StorybookContainer.js
+++ src/components/storybook/StorybookContainer.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Container,Button,Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Storybook, { CarouselItem } from "./Storybook";
-
+import StoryBookSidePanel from "../sidepanels/StoryBookSidePanel";
 
 const StorybookContainer = (props) => {
   const name = props.name;
   console.log(name);
   return (
-      <div style={{textAlign:'left', paddingLeft:'1rem'}} className="workbook">
-        <Storybook>
+    <div
+      className="StoryBookContainer"
+    >
+      <Storybook>
         <CarouselItem>
           <h3>Dinosaurs</h3>
         </CarouselItem>
@@ -21,22 +21,8 @@ const StorybookContainer = (props) => {
           <h3>Third Slide Label</h3>
         </CarouselItem>
       </Storybook>
-          <Container>
-            <Row style={{margin:"0.3rem", padding:"0.2rem",border: "2px solid rgb(177, 181, 182)",alignItems: 'center'}}>
-                <Col md={4}>
-                    {name}
-                </Col>
-                <Col md={{ span: 4, offset: 4 }}>
-                    <Button variant="outline-primary">Analytics</Button>{' '}
-                    <Link to="/storybook">
-                        <Button variant="primary">Open</Button>{' '}
-                    </Link>
-                </Col>
-            </Row>
-          </Container>
-          
-        
-        </div>
+      <StoryBookSidePanel /> 
+    </div>
   );
 };
 
