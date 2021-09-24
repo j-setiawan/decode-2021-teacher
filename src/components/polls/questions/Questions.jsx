7a5d1aad0957fc7f4eda410df407436ad6c9df7d
Anas Shahid <muhammad.shahid@ericsson.com>
2021-09-24 15:45:35 -0400

    finalized polling modal

--- src/components/polls/questions/Questions.jsx
+++ src/components/polls/questions/Questions.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import { UIStore } from "../../../store";
 import { v4 as uuidv4 } from "uuid";
+import Modal from "react-modal";
 import "./Questions.css";
 
 const Questions = (props) => {
   const [question, setQuestion] = useState("");
   const [response, setResponse] = useState("");
   const [responses, setResponses] = useState([]);
+
+  const [modalIsOpen, setIsOpen] = React.useState(true);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   const submitClick = () => {
     const output = {
@@ -32,41 +43,52 @@ const Questions = (props) => {
     setResponses(newArray);
   };
   return (
-    <div className="questionCard">
-      <div className="first">
-        <h3>Enter your question</h3>
-        <input
-          type="text"
-          onChange={(e) => setQuestion(e.target.value)}
-          placeholder="Question"
-        />
-      </div>
-      <div className="second">
-        <h3>Enter your poll option</h3>
-        <div className="inner-second">
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+    >
+      <div className="questionCard">
+        <div className="first">
+          <h3>Enter your question</h3>
           <input
             type="text"
-            placeholder="Poll option"
-            onChange={(e) => setResponse(e.target.value)}
-            value={response}
+            onChange={(e) => setQuestion(e.target.value)}
+            placeholder="Question"
           />
-          <button onClick={addPollItem}>Add Poll Item</button>
+        </div>
+        <div className="second">
+          <h3>Enter your poll option</h3>
+          <div className="inner-second">
+            <input
+              type="text"
+              placeholder="Poll option"
+              onChange={(e) => setResponse(e.target.value)}
+              value={response}
+            />
+            <button onClick={addPollItem}>Add Poll Item</button>
+          </div>
+        </div>
+
+        <div className="polls">
+          <div className="response">
+            <h3 className="pollOptions">Poll Options</h3>
+            {responses.map((response, idx) => (
+              <div key={idx} className="pollOption">
+                <p>{response}</p>
+                <button onClick={() => deleteResponse(response)}>Delete</button>
+              </div>
+            ))}
+          </div>
+
+          <div className="button">
+            <button className="createPoll" onClick={submitClick}>
+              Create Poll
+            </button>
+          </div>
         </div>
       </div>
-
-      <h3>Poll Options</h3>
-      {responses.map((response, idx) => (
-        <div key={idx} className="pollOption">
-          <p>{response}</p>
-          <button onClick={() => deleteResponse(response)}>Delete</button>
-        </div>
-      ))}
-      <br />
-
-      <button className="createPoll" onClick={submitClick}>
-        Create Poll
-      </button>
-    </div>
+    </Modal>
   );
 };
 export default Questions;
