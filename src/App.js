222d524c11ca2eb3fd59af69b38583055598a721
Habiba Mostafa <mostafah@uoguelph.ca>
2021-09-24 09:52:05 -0400

    Added workbook buttons + data and time

--- src/App.js
+++ src/App.js
@@ -1,31 +1,85 @@
-import "./App.css";
-import Emotions from "./components/emotions/Emotions";
-import Questions from "./components/polls/questions/Questions";
-import Storybook, { CarouselItem } from "./components/storybook/Storybook";
+import './App.css';
+import Questions from './components/polls/questions/Questions';
+import Storybook, { CarouselItem } from './components/storybook/Storybook';
+import DateTime from './components/storybook/DateTime';
+import styled from 'styled-components';
+import React, { useState } from "react";
+
+const headerButtons = ['Start Lesson Plan', 'End Lesson Plan'];
+const footerButtons = ['Post Workbook'];
+
 
 function App() {
-  const students = {
-    totalStudentsHappy: 1,
-    totalStudentsNeutral: 15,
-    totalStudentsSad: 23,
-  };
+  const question = {
+    pollId: "123",
+    question: "What is the name of the hackathon?",
+    choices: [
+      {
+        choiceId: "435",
+        description: "Decode"
+      },
+      {
+        choiceId: "4356",
+        description: "Newcode"
+      }
+    ]
+  }
+
   return (
     <div className="App">
-      <Questions />
+      <Questions props={question} />
+      <DateTime>
+      </DateTime>
+      <ToggleGroup buttonTags = {headerButtons}/>
       <Storybook>
         <CarouselItem>
-          <h3>Dinosaurs</h3>
+          <h3>Agenda for teacher </h3>
         </CarouselItem>
         <CarouselItem>
           <h3>Second Slide Label</h3>
         </CarouselItem>
         <CarouselItem>
-          <h3>Third Slide Label</h3>
+        <h3>Third Slide Label</h3>
         </CarouselItem>
       </Storybook>
-      <Emotions students={students} />
+      <ToggleGroup buttonTags = {footerButtons}/>
     </div>
   );
 }
 
 export default App;
+
+const Button = styled.button`
+`;
+
+const ButtonToggle = styled(Button)`
+  opacity: 0.6;
+  height: 40px;
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1;
+  `}
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+function ToggleGroup( {buttonTags} ) {
+  const [active, setActive] = useState(buttonTags[0]);
+  return (
+    <ButtonGroup>
+      {buttonTags.map(type => (
+        <ButtonToggle
+          key={type}
+          active={active === type}
+          onClick={() => setActive(type)}
+        >
+          {type}
+        </ButtonToggle>
+      ))}
+    </ButtonGroup>
+  );
+}
