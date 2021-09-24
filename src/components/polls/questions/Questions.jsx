f6718eb4b83fa1c0358feef43dd5bcb6bde6937f
Anas Shahid <muhammad.shahid@ericsson.com>
2021-09-24 01:49:07 -0400

    updated the styling for form

--- src/components/polls/questions/Questions.jsx
+++ src/components/polls/questions/Questions.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 import { UIStore } from "../../../store";
 import { v4 as uuidv4 } from "uuid";
+import "./Questions.css";
 
 const Questions = (props) => {
   const [question, setQuestion] = useState("");
   const [response, setResponse] = useState("");
-  const [response_2, setResponse_2] = useState("");
-  const [response_3, setResponse_3] = useState("");
+  const [responses, setResponses] = useState([]);
 
   const submitClick = () => {
     const output = {
       id: uuidv4(),
       question,
-      response,
-      response_2,
-      response_3,
+      responses,
     };
 
     //store the poll
@@ -23,33 +21,51 @@ const Questions = (props) => {
       console.log(UIStore);
     });
   };
+
+  const addPollItem = () => {
+    setResponses((oldArray) => [...oldArray, response]);
+    setResponse("");
+  };
+
+  const deleteResponse = (response) => {
+    const newArray = responses.filter((r) => r !== response);
+    setResponses(newArray);
+  };
   return (
-    <div>
-      <label>Enter your question?</label>
-      <input type="text" onChange={(e) => setQuestion(e.target.value)} />
+    <div className="questionCard">
+      <div className="first">
+        <h3>Enter your question</h3>
+        <input
+          type="text"
+          onChange={(e) => setQuestion(e.target.value)}
+          placeholder="Question"
+        />
+      </div>
+      <div className="second">
+        <h3>Enter your poll option</h3>
+        <div className="inner-second">
+          <input
+            type="text"
+            placeholder="Poll option"
+            onChange={(e) => setResponse(e.target.value)}
+            value={response}
+          />
+          <button onClick={addPollItem}>Add Poll Item</button>
+        </div>
+      </div>
+
+      <h3>Poll Options</h3>
+      {responses.map((response, idx) => (
+        <div key={idx} className="pollOption">
+          <p>{response}</p>
+          <button onClick={() => deleteResponse(response)}>Delete</button>
+        </div>
+      ))}
       <br />
-      <label>Enter your response</label>
-      <input
-        type="text"
-        placeholder="response 1"
-        onChange={(e) => setResponse(e.target.value)}
-      />
-      <br />
-      <label>Enter your response</label>
-      <input
-        type="text"
-        placeholder="response 2"
-        onChange={(e) => setResponse_2(e.target.value)}
-      />
-      <br />
-      <label>Enter your response</label>
-      <input
-        type="text"
-        placeholder="response 3"
-        onChange={(e) => setResponse_3(e.target.value)}
-      />
-      <br />
-      <button onClick={submitClick}>Create Poll</button>
+
+      <button className="createPoll" onClick={submitClick}>
+        Create Poll
+      </button>
     </div>
   );
 };
